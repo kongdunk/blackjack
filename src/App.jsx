49b183b256c920b068d43e82c963d4a7f8dce77b
kongdunk <daesan98@gmail.com>
2023-01-25 20:36:27 -0800

    getting player card function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [deckID, setDeckID] = useState("")
+  const [playerHand, setPlayerHand] = useState([])
+  const [houseHand, setHouseHand] = useState([])
 
+  const deckUrl = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+  const drawCardUrl = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`
+
+  // calls API for a deck of shuffled cards
+  const getDeck = () => {
+    axios.get(deckUrl)
+      .then((response) => {
+        setDeckID(response.data.deck_id);
+      })
+  }
+  
+  // calls API to draw a card from the deck and adds the card to the playerHand Array
+  const getPlayerCard = () => {
+    axios.get(drawCardUrl)
+      .then((response) => {
+        setPlayerHand(oldArray => [...oldArray, response.data.cards]);
+        console.log(playerHand)
+        console.log(playerHand[0][0].value)
+      })
+  }
+  // After Player Hits Once
+  if(playerHand.length > 0){
+    return (
+      <>
+        {
+          playerHand.map((e) =>
+            <div>{e[0].value} </div>
+          )
+        }
+        
+        <button onClick={() => getPlayerCard()}> Hit </button>
+        <button onClick={() => getPlayerCard()}> Stand </button>
+      </>
+    )
+  }
+  // Initial Screen
   return (
-    <div className="App">
+    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={() => getDeck()}> get deck </button>
+        <button onClick={() => getPlayerCard()}> Hit </button>
+        <button onClick={() => getPlayerCard()}> Stand </button>    
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </>
   )
 }
 
